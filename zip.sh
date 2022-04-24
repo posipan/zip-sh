f84fdec1c27948f7960a37a9781fa91224e49018
shingo.sugure <gonshi555dashiyo@gmail.com>
2022-04-24 16:38:37 +0900

    fix except option

--- zip.sh
+++ zip.sh
@@ -4,4 +4,4 @@ THEME_NAME=[THEME_NAME]
 
 cd [OPTION_PATH]/${THEME_NAME}/dev
 
-zip -r [OPTION_PATH]/${THEME_NAME}.zip ${THEME_NAME}/ -x "*.git*" "*node_modules*" "*.DS_Store*" "*..vscode*"
+zip -r [OPTION_PATH]/${THEME_NAME}.zip ${THEME_NAME}/ -x "*.git*" "*node_modules*" "*.DS_Store*" "*.vscode*"
