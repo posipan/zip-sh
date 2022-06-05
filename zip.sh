04486f8c1ba314672fe7dfc07e56dc63f5db7140
shingo.sugure <gonshi555dashiyo@gmail.com>
2022-06-05 09:09:49 +0900

    fix

--- zip.sh
+++ zip.sh
@@ -1,7 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-THEME_NAME=[THEME_NAME]
+# 変数宣言
+BASE=zip-sh
+TARGET=sample
+DELIVERY=delivery
 
-cd [OPTION_PATH]/${THEME_NAME}/dev
+BASE_PATH=/Users/sugure/Documents/web/utility/${BASE}
 
-zip -r [OPTION_PATH]/${THEME_NAME}.zip ${THEME_NAME}/ -x "*.git*" "*node_modules*" "*.DS_Store*" "*.vscode*"
+# deliveryフォルダが存在しなれば、deliveryフォルダを作成する
+mkdir -p ${BASE_PATH}/${DELIVERY}
+
+# 対象ディレクトリまで移動する
+cd ${BASE_PATH}/${TARGET}
+
+# node_modulesやGit関連ファイル、.DS_Storeを除いてzipファイルを作成する
+# 今回の場合、deliveryフォルダ内にsample.zipというファイルが作成される
+zip -r ${BASE_PATH}/${DELIVERY}/${TARGET}.zip * -x "*node_modules*" ".git*" ".DS_Store"
